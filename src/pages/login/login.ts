4f860ceba676629a43a41803906ad1b1006a8056
kfm12 <40537649+kfm12@users.noreply.github.com>
2018-06-27 23:38:33 +0200

    Update login.ts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,9 +14,20 @@ import { ProfilePage } from '../profile/profile';
  templateUrl: 'login.html',
 })
 export class LoginPage {
+  // public email: string; //
+  // public password: string; //
 
- constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  // public names: Array<string>; //
+  // public names2: string[]; //
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // this.email = "kfm12@duke.edu"; //
+  }
+  // pressMe(argument1: string, argument2: number){ //
+  //   console.log("This email is" + this.email); //
+  //   console.log("Argument 1:", argument1); //
+  //   console.log("Argument 2:", argument2); //
+  // } //
   navigateToProfile() {
     console.log("Navigating...");
 
@@ -28,3 +39,40 @@ export class LoginPage {
  }
 
 }
+
+// Class overview / object-oriented programming
+
+// class Animal {
+//   type: string;
+//   age: number;
+
+//   constructor(type:string) {
+//     this.type = type;
+//     this.type = "I am a dog";
+
+//     this.age = 1;
+//     this.age = 5.6;
+//   }
+
+//   makeNoise() {
+//     console.log("Making noise");
+//   }
+// }
+
+// class Dog extends Animal {
+
+//   someAnimal: Animal;
+
+//   constructor(){
+//     // new Animal ("dog)");
+//     super("dog");
+//   }
+// }
+
+// var dog = new Animal("dog");
+// dog.makeNoise();
+
+// var cat = new Animal("cat");
+
+// var dog = new Animal("dog");
+// var doggy = new Dog();
