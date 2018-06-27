3f031bec9e380c6dec0e16fd570a6533cc00e4b6
kfm12 <40537649+kfm12@users.noreply.github.com>
2018-06-27 23:36:40 +0200

    Create products.ts

---
+++ src/pages/products/products.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams } from 'ionic-angular';
+
+@Component({
+ selector: 'page-products',
+ templateUrl: 'products.html',
+})
+export class ProductsPage {
+  public name: string;
+  public note: string;
+  public price: number;
+  public description: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  pressMe(arg1: string, arg2: string, arg3: number, arg4: string) {
+    console.log("Printing...maybe. What is going on?")
+  }
+ 
+
+ ionViewDidLoad() {
+   console.log('ionViewDidLoad AboutPage');
+  }
+ public items: ProductsPage [
+  {"name": "Mrs. Quibbleton", "note": "C", "price": 19.95, "description": "Your typical mother hen. Likes movie nights and nesting with her brood."},
+  {"name": "Heathersby", "note": "D", "price": 19.95, "description": "Slightly skittish middle-aged male. Squawks a wonderdul tenor D."},
+  {"name": "Gallina", "note": "E", "price": 19.95, "description": "Young female soprano. A bit of a diva but well worth it."},
+  ];
+   
+}
+
+
+
