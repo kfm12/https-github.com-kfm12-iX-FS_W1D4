4feb7995e83a79f5f6db76cfd2499aa497880339
kfm12 <40537649+kfm12@users.noreply.github.com>
2018-06-28 21:45:02 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,28 @@ import { LoginPage } from '../pages/login/login';
 import { ProfilePage } from '../pages/profile/profile';
 import { RegistrationPage } from '../pages/registration/registration';
 import { ProductsPage } from '../pages/products/products';
+import { ProductPage } from '../pages/product/product';
 import { AboutPage } from '../pages/about/about';
+import { ProductService } from '../services/product.service'
+import { CartPage } from '../pages/cart/cart';
+import { CheckOutPage } from '../pages/check-out/check-out';
+import { PaymentPage } from '../pages/payment/payment';
+import { RegConfirmPage } from '../pages/reg-confirm/reg-confirm';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ProductsPage,
+    ProductPage,
     AboutPage,
+    CartPage,
+    CheckOutPage,
     LoginPage,
     ProfilePage,
-    RegistrationPage
+    RegistrationPage,
+    RegConfirmPage,
+    PaymentPage
   ],
   imports: [
     BrowserModule,
@@ -31,14 +42,20 @@ import { AboutPage } from '../pages/about/about';
     MyApp,
     HomePage,
     ProductsPage,
+    ProductPage,
     AboutPage,
+    CartPage,
+    CheckOutPage,
     LoginPage,
     ProfilePage,
-    RegistrationPage
+    RegistrationPage,
+    RegConfirmPage,
+    PaymentPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ProductService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
